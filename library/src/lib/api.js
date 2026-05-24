e6b594523af9fc9dd56356dee2d669cd1d71c087
Prajwal Rai <raihangrim064@gmail.com>
2026-05-24 14:14:39 +0545

    completed

--- library/src/lib/api.js
+++ library/src/lib/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
     (import.meta.env.PROD
-      ? 'https://library-1-e1mi.onrender.com'
+      ? 'https://library-hsp2.onrender.com'
       : 'http://localhost:5000'),
 });
 
